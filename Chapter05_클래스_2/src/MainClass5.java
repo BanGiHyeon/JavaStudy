b1a3d092259f2af3ffc7c7b2266a1584601f885f
SIST <SIST@DESKTOP-85FEDHD>
2023-10-10 17:51:07 +0900

    171717

---
+++ Chapter05_클래스_2/src/MainClass5.java
@@ -0,0 +1,91 @@
+class 영화
+{
+	int movieID;
+	String poster,nameKr,nameEn;
+	String regdate;
+	String genre;
+	String nation;
+	String grade;
+	String time;
+	double score;
+	int boxoffice;
+}
+class 뉴스
+{
+	int news_no;
+	String poster;
+	String title;
+	String content;
+	String regdate;
+	String author;
+}
+class Foodcategory
+{
+	int food_cate_no;
+	String poster;
+	String title;
+	String subject;
+}
+class 맛집
+{
+	int fno;
+	String title;
+	double score;
+	String address;
+	String phone;
+	String type;
+	String price;
+	String parking;
+	String time;
+	String menu;
+}
+class Recipe
+{
+	int recipe_no;
+	String poster;
+	String title;
+	String chef;
+	int score;
+	int hit;
+	int showUser;
+}
+class music
+{
+	int music_no;
+	int rank;
+	String state;
+	int idcrement;
+	String poster;
+	String title;
+	String singer;
+	String album;
+	int like;
+}
+class goods
+{
+	int goods_no;
+	String goods_discount;
+	String goods_poster;
+	String goods_name;
+	String goods_price;
+}
+class theater
+{
+	int no;
+	String poster;
+	String title;
+	String genre;
+	String regdate;
+	String location;
+	String actor;
+	String grade;
+	String time;
+}
+public class MainClass5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
